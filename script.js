84c9f158eed147129892f8ef125a7e5ae6c457e1
hose347 <hosse.css@gmail.com>
2022-03-02 12:39:50 +0100

    html and styles

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
