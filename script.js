3a33a5d974d33448220b776507f29cb0cb9e4b9f
hose347 <hosse.css@gmail.com>
2022-03-20 20:01:32 +0100

    async / defer / regular

--- script.js
+++ script.js
@@ -394,3 +394,16 @@ document.querySelector('.nav').addEventListener(
   true
 );
 */
+// document.addEventListener('DOMContentLoaded', function (e) {
+//   console.log('HTML parsed and DOM tree build', e);
+// });
+
+// window.addEventListener('load', function (e) {
+//   console.log('Page fully loaded', e);
+// });
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
